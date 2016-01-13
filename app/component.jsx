33a716eaf57c03ca405e6eb8d93e3c6d48b874f1
Mariya Gnitetskaya <tennalian@gmail.com>
2016-01-13 17:37:56 +0200

    webpack config

---
+++ app/component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// require('styles.scss');
+
+export default class Hello extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: ['webpack-dev-server/client', 'webpack/hot/dev-server', './app/main'],
+	output: {
+		path: './public',
+		filename: 'build.js'
+	},
+	resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+	module: {
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				},
+				exclude: /\/node_modules/
+			},
+			{
+				test: /\.scss$/,
+				// loader: ExtractTextPlugin.extract('style-loader', 'css-loader','sass-loader')
+				loader: 'style-loader!css-loader!sass-loader'
+			},
+			{
+		    	test: /\.(png|jpg|svg|ttf|woff|woff2|eot)$/,
+		    	loader: 'file?name=[path][name].[ext]',
+		    	exclude: /\/node_modules/
+		    },
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('styles.css')
+	],
+	devServer:{
+		contentBase: './'
+    }
+};
